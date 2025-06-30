de3f9dcd88f13e8aa9fe5a4cb0788a21eb3e99fd
Sina Huster <sina@huster.me.uk>
2025-06-30 14:38:32 +0000

    Mon, Jun 30, 2025, 4:38 PM +02:00

---
+++ Week1/cat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int get_positive_int(void);
+void meow(int n);
+
+int main(void)
+{
+    int times = get_positive_int();
+    meow(times);
+}
+
+int get_positive_int(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number: ");
+    }
+    while(n < 1);
+
+    return n;
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
