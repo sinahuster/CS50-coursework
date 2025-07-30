746a34b171426d9f972b9eb74cbfde49ecd57ba3
Sina Huster <sina@huster.me.uk>
2025-07-30 12:48:24 +0000

    Wed, Jul 30, 2025, 2:48 PM +02:00

--- Week4/Lecture/copy/copy.c
+++ Week4/Lecture/copy/copy.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
@@ -9,12 +10,17 @@ int main(void)
 
     char *t = malloc(strlen(s) + 1);
 
-    for (int i = 0; n < strlen(s): i < n; i ++)
+    if (t == NULL)
     {
-        t[i] = s[i];
+        return 1;
     }
 
-    t[0] = toupper(t[0]);
+    strcpy(t, s);
+    
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
 
     printf("%s\n", s);
     printf("%s\n", t);
