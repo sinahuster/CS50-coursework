b29768ee6b16f7a22611ca893635e6d53f4b6be5
Sina Huster <sina@huster.me.uk>
2025-08-13 12:49:33 +0000

    Wed, Aug 13, 2025, 2:49 PM +02:00

--- Week6/AdditionalProblems/figlet/figlet.py
+++ Week6/AdditionalProblems/figlet/figlet.py
@@ -1,13 +1,16 @@
 import sys
+import random
 from pyfiglet import Figlet
 
 figlet = Figlet()
 fonts = figlet.getFonts()
 
 if len(sys.argv) == 1:
-    #random font
+    font = random.choice(fonts)
 elif len(sys.argv) == 3 and (argv[1] == '-f' or argv[1] == '--font'):
     font = sys.argv[2]
 else:
     print("Invalid usage")
     sys.exit(1)
+
+
