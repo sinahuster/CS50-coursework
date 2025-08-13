fdee7052d105f8287fe596ce7841f32527594faa
Sina Huster <sina@huster.me.uk>
2025-08-13 12:06:09 +0000

    Wed, Aug 13, 2025, 2:06 PM +02:00

---
+++ Week6/AdditionalProblems/bank.py
@@ -0,0 +1,10 @@
+greeting = str(input("Greeting: "))
+
+contains_hello = False
+contains_h = False
+
+for word in greeting:
+    if word == 'hello':
+        
+    for character in word:
+        if character is
