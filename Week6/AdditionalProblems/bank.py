b8f20dbce9e0f59481677cc4a7469e685bdebfb8
Sina Huster <sina@huster.me.uk>
2025-08-13 12:35:33 +0000

    Wed, Aug 13, 2025, 2:35 PM +02:00

--- Week6/AdditionalProblems/bank.py
+++ Week6/AdditionalProblems/bank.py
@@ -3,9 +3,10 @@ import string
 # get the greeting input from the user
 greeting = str(input("Greeting: ")).strip()
 
-# determine the first word of the greeting
+# determine the first word of the greeting and remove any attached punctuation
 first_word = greeting.split()[0].strip(string.punctuation)
 
+# check with coniditon applies and the dollars associated 
 if first_word.lower() == 'hello':
     dollars = 0
 elif first_word.lower().startswith('h'):
