a264d6b3d269f9bc7d193b51f0b782f38305e1c9
Sina Huster <sina@huster.me.uk>
2025-08-13 12:26:51 +0000

    Wed, Aug 13, 2025, 2:26 PM +02:00

--- Week6/AdditionalProblems/bank.py
+++ Week6/AdditionalProblems/bank.py
@@ -1,9 +1,12 @@
-greeting = str(input("Greeting: ")).split()
+greeting = str(input("Greeting: ")).strip()
 
 first_word = greeting.split()[0]
 
 if first_word.lower() == 'hello':
     dollars = 0
-elif first_word.lower().startswith('h)
+elif first_word.lower().startswith('h'):
+    dollars = 20
+else:
+    dollars = 100
 
 print(f"$ {dollars}")
