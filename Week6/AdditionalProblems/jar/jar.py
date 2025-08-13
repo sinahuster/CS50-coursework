87c507d6d5ed1eca6662f74b7fa1235beba8b01a
Sina Huster <sina@huster.me.uk>
2025-08-13 13:51:50 +0000

    Wed, Aug 13, 2025, 3:51 PM +02:00

--- Week6/AdditionalProblems/jar/jar.py
+++ Week6/AdditionalProblems/jar/jar.py
@@ -1,9 +1,10 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
+        self.size = 0
 
     def __str__(self):
-        return self.size * "🍪"
+        return "🍪" * self.size
 
     def deposit(self, n):
         pass
