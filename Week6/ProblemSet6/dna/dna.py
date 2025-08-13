c55b93972ce850a00bd46107e2a1f76c8da398e6
Sina Huster <sina@huster.me.uk>
2025-08-13 09:55:33 +0000

    Wed, Aug 13, 2025, 11:55 AM +02:00

--- Week6/ProblemSet6/dna/dna.py
+++ Week6/ProblemSet6/dna/dna.py
@@ -9,13 +9,22 @@ def main():
         print("Missing comand line arguement. Expected ./dna cvs-file tex-file")
         sys.exit(1)
 
+    data = []
     # Read database file into a variable
-    with open(argv[1], "r") as data_file:
-        
+    with open(sys.argv[1], 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row)
 
-    # TODO: Read DNA sequence file into a variable
 
-    # TODO: Find longest match of each STR in DNA sequence
+    # Read DNA sequence file into a variable
+    with open(sys.argv[2], 'r') as file:
+        dna = file.readline().strip()
+
+
+    # Find longest match of each STR in DNA sequence
+    
+    print(csv_reader.fieldnames)
 
     # TODO: Check database for matching profiles
 
