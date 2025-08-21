76232d9446b6876c1190031fa19e08dd46131441
Sina Huster <sina@huster.me.uk>
2025-08-21 12:52:26 +0000

    Thu, Aug 21, 2025, 2:52 PM +02:00

--- Week7/AdditionalProblem/prophecy/prophecy.py
+++ Week7/AdditionalProblem/prophecy/prophecy.py
@@ -9,7 +9,7 @@ db = SQL("sqlite:///new_roster.db")
 # Create table
 db.execute("CREATE TABLE students(id INTEGER, name TEXT, PRIMARY KEY(id))")
 db.execute("CREATE TABLE houses(id INTEGER, house TEXT, house_head TEXT, PRIMARY KEY(id))")
-db.execute("CREATE TABLE house_assignements(student_id INTEGER, house_id INTEGER, FORGEIN KEY(student_id) REFERENCES student(id), FORGEIN KEY(house_id) REFERENCES houses(id))")
+db.execute("CREATE TABLE house_assignments(student_id INTEGER, house_id INTEGER, FOREIGN KEY(student_id) REFERENCES students(id), FOREIGN KEY(house_id) REFERENCES houses(id))")
 
 # Check table contents
 print(db.execute("SELECT * FROM students"))
