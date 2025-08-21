29ff2f9eab263e0214f0c2330cfd3247d53abd19
Sina Huster <sina@huster.me.uk>
2025-08-21 12:46:14 +0000

    Thu, Aug 21, 2025, 2:46 PM +02:00

--- Week7/AdditionalProblem/prophecy/prophecy.py
+++ Week7/AdditionalProblem/prophecy/prophecy.py
@@ -1,9 +1,9 @@
 from cs50 import SQL
 
 # Open database
-db = SQL("sqlite:///roster.db")
+db = SQL("sqlite:///new_roster.db")
 
 # Create table
-student = db.execute("CREATE TABLE students(id INTEGER, name TEXT, PRIMARY KEY(id))")
-house = db.execute("CREATE TABLE houses(id INTEGER, name TEXT, PRIMARY KEY(id))")
-
+db.execute("CREATE TABLE students(id INTEGER, name TEXT, PRIMARY KEY(id))")
+db.execute("CREATE TABLE houses(id INTEGER, house TEXT, house_head TEXT, PRIMARY KEY(id))")
+db.execute("CREATE TABLE house_assignements(student_id INTEGER, house_id INTEGER, FORGEIN KEY(student_id)))
