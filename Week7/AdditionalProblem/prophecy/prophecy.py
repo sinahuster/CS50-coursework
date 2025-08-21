74a7fa5198df69309c048e591f0d511e5cb8d5bd
Sina Huster <sina@huster.me.uk>
2025-08-21 12:34:51 +0000

    Thu, Aug 21, 2025, 2:34 PM +02:00

--- Week7/AdditionalProblem/prophecy/prophecy.py
+++ Week7/AdditionalProblem/prophecy/prophecy.py
@@ -6,3 +6,4 @@ db = SQL("sqlite:///roster.db")
 # Create table
 student = db.execute("CREATE TABLE students(id INTEGER, name TEXT, PRIMARY KEY(id))")
 house = db.execute("CREATE TABLE houses(id INTEGER, name TEXT, PRIMARY KEY(id))")
+
