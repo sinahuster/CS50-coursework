a114f2bee693ce6b0e9450663f4de38a190a5a22
Sina Huster <sina@huster.me.uk>
2025-08-21 12:49:26 +0000

    Thu, Aug 21, 2025, 2:49 PM +02:00

--- Week7/AdditionalProblem/prophecy/prophecy.py
+++ Week7/AdditionalProblem/prophecy/prophecy.py
@@ -6,5 +6,4 @@ db = SQL("sqlite:///new_roster.db")
 # Create table
 db.execute("CREATE TABLE students(id INTEGER, name TEXT, PRIMARY KEY(id))")
 db.execute("CREATE TABLE houses(id INTEGER, house TEXT, house_head TEXT, PRIMARY KEY(id))")
-db.execute("CREATE TABLE house_assignements(student_id INTEGER, house_id INTEGER,
-           FORGEIN KEY(student_id) REFERENCES student(id), FORGEIN KEY(house_id) REFERENCES houses(id)))
+db.execute("CREATE TABLE house_assignements(student_id INTEGER, house_id INTEGER, FORGEIN KEY(student_id) REFERENCES student(id), FORGEIN KEY(house_id) REFERENCES houses(id))")
