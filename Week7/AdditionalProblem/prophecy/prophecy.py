9e86656897e533280a251c9b1dfe00a14d58402e
Sina Huster <sina@huster.me.uk>
2025-08-22 12:41:38 +0000

    Fri, Aug 22, 2025, 2:41 PM +02:00

--- Week7/AdditionalProblem/prophecy/prophecy.py
+++ Week7/AdditionalProblem/prophecy/prophecy.py
@@ -13,15 +13,18 @@ db.execute("CREATE TABLE IF NOT EXISTs houses(id INTEGER, house TEXT UNIQUE, hou
 db.execute("CREATE TABLE IF NOT EXISTS house_assignments(student_id INTEGER, house_id INTEGER, FOREIGN KEY(student_id) REFERENCES students(id), FOREIGN KEY(house_id) REFERENCES houses(id))")
 
 data = []
-houses = []
+houses = {}
+
 with open('students.csv', 'r') as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
         data.append(row)
 
 for row in data:
-    db.execute("INSERT INTO students (id, name) values (?, row['name'])", row['id'])
-    db.execute("INSERT INTO houses (id, house, house_head) values(row)")
+    db.execute("INSERT INTO students (id, name) values (?, ?)", row['id'], row['name'])
+    if not row['house'] in houses:
+        db.execute("INSERT INTO houses (house, house_head) values(?, ?)", row['house'], row['head'])
+        house_id = db.execute 
 
 # Check table contents
 #print(db.execute("SELECT * FROM students"))
