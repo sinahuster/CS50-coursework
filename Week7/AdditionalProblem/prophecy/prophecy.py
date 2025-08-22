7798ab1c82e4f9ac3b68c7d7148dc4c3e5fabf1b
Sina Huster <sina@huster.me.uk>
2025-08-22 12:22:10 +0000

    Fri, Aug 22, 2025, 2:22 PM +02:00

--- Week7/AdditionalProblem/prophecy/prophecy.py
+++ Week7/AdditionalProblem/prophecy/prophecy.py
@@ -12,9 +12,17 @@ db.execute("CREATE TABLE IF NOT EXISTS students(id INTEGER, name TEXT, PRIMARY K
 db.execute("CREATE TABLE IF NOT EXISTs houses(id INTEGER, house TEXT, house_head TEXT, PRIMARY KEY(id))")
 db.execute("CREATE TABLE IF NOT EXISTS house_assignments(student_id INTEGER, house_id INTEGER, FOREIGN KEY(student_id) REFERENCES students(id), FOREIGN KEY(house_id) REFERENCES houses(id))")
 
-data = open("students.csv", )
+data = []
+with open('students.csv', 'r') as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        data.append(row)
+
+for row in data:
+    db.execute("INSERT INTO students (id, name) values (row['id'], row['name'])")
+    db.execute("INSERT INTO houses (id, house, house_head) values(row))
 
 # Check table contents
-print(db.execute("SELECT * FROM students"))
-print(db.execute("SELECT * FROM houses"))
-print(db.execute("SELECT * FROM house_assignments"))
+#print(db.execute("SELECT * FROM students"))
+#print(db.execute("SELECT * FROM houses"))
+#print(db.execute("SELECT * FROM house_assignments"))
