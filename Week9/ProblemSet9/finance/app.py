e23276535ac884d13762a45d429dc3fc597a7f16
Sina Huster <sina@huster.me.uk>
2025-09-16 13:00:47 +0000

    Tue, Sep 16, 2025, 3:00 PM +02:00

--- Week9/ProblemSet9/finance/app.py
+++ Week9/ProblemSet9/finance/app.py
@@ -157,7 +157,7 @@ def register():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("register.html")
+       return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
